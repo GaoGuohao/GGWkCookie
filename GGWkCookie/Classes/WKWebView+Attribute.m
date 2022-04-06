a829d357758f55666429c757c270d3c80db30c9a
GarrettGao <gaoguohao@jiehun.com.cn>
2022-04-06 15:45:22 +0800

    runtime拓展 weak属性优化

--- GGWkCookie/Classes/WKWebView+Attribute.m
+++ GGWkCookie/Classes/WKWebView+Attribute.m
@@ -8,16 +8,32 @@
 #import "WKWebView+Attribute.h"
 #import <objc/runtime.h>
 
+@interface WeakObjectContainer : NSObject
+@property (nonatomic, readonly, weak) id object;
+@end
+
+@implementation WeakObjectContainer
+- (instancetype) initWithObject:(id)object
+{
+    if (!(self = [super init])) {return nil;}
+    _object = object;
+    return self;
+}
+
+@end
+
 @implementation WKWebView (Attribute)
 
 static NSString *delegatePointer;
 
 - (void)setCookieDelegate:(id<GGWkWebViewDelegate>)cookieDelegate {
-    objc_setAssociatedObject(self, &delegatePointer, cookieDelegate, OBJC_ASSOCIATION_ASSIGN);
+    objc_setAssociatedObject(self, &delegatePointer, [[WeakObjectContainer alloc] initWithObject:cookieDelegate],
+                             OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (id<GGWkWebViewDelegate>)cookieDelegate {
-    return objc_getAssociatedObject(self, &delegatePointer);
+    WeakObjectContainer *container = objc_getAssociatedObject(self, &delegatePointer);
+    return container.object;
 }
 
 
